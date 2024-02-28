d3cb0ff7002f77b9c840613a55d1a4a7d94e7482
m7md47282 <49171345+m7md47282@users.noreply.github.com>
2024-02-28 21:19:21 +0300

    Add files via upload
    push project

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent implements OnInit { 
+  loginForm: FormGroup;
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/)]]
+    });
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted successfully');
+      console.log('Email:', this.loginForm.value.email);
+      console.log('Password:', this.loginForm.value.password);
+    } else {
+      console.log('Form has validation errors');
+    }
+      
+    this.resetFrom()
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+  }
+
+  resetFrom() {
+    this.loginForm.reset()
+  }
+
+}
